56c6badeef1c8991935bdfeeebd20a734cf22ad3
Suman.Majumdar <sumanmajumdar13@gmail.com>
2020-09-12 17:58:16 -0500

    Initial Commit

---
+++ TweetBook/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TweetBook.Controllers
+{
+    public class TestController: Controller
+    {
+        [HttpGet("api/test")]
+        public IActionResult Get()
+        {
+            return Ok(new { name = "From TestController" });
+        }
+    }
+}
