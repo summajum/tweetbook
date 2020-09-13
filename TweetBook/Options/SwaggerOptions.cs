54cad36786a836d407c374ddc4f05d682de20e61
Suman.Majumdar <sumanmajumdar13@gmail.com>
2020-09-12 20:21:07 -0500

    Chapter 2 Completed

--- TweetBook/Options/SwaggerOptions.cs
+++ TweetBook/Options/SwaggerOptions.cs
@@ -9,7 +9,6 @@ namespace TweetBook.Options
     public class SwaggerOptions
     {
         public string JsonRoute { get; set; }
-
         public string Description { get; set; }
         public string UIEndpoint { get; set; }
     }
