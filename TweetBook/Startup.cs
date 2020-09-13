e7fd74a3748518ad34dbacc00d820525a0cefd4f
Suman.Majumdar <sumanmajumdar13@gmail.com>
2020-09-12 19:29:51 -0500

    Chapter 1 Completed

--- TweetBook/Startup.cs
+++ TweetBook/Startup.cs
@@ -12,6 +12,8 @@ using TweetBook.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using TweetBook.Options;
+using Microsoft.OpenApi.Models;
 
 namespace TweetBook
 {
@@ -32,6 +34,9 @@ namespace TweetBook
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<DataContext>();
+            services.AddSwaggerGen(x => {
+                x.SwaggerDoc("v1", new OpenApiInfo { Title = "TweetBook Api Swagger Doc", Description = "TweetBook Api Swagger Desc", Version = "TweetBook Api Swagger V1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,6 +51,18 @@ namespace TweetBook
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            var swaggerOptions = new SwaggerOptions();
+            Configuration.GetSection(nameof(SwaggerOptions)).Bind(swaggerOptions);
+            app.UseSwagger(option =>
+            {
+                option.RouteTemplate = swaggerOptions.JsonRoute;
+            });
+            app.UseSwaggerUI(option => {
+                option.SwaggerEndpoint(swaggerOptions.UIEndpoint, swaggerOptions.Description);
+            });
+
+                
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
